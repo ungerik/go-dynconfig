c7fd8eb85439aba5ed9325f5e471f48f136d6c83
Erik Unger <erik@erikunger.com>
2022-09-19 10:22:21 +0200

    single loadfuncs.go file

--- loadfuncs.go
+++ loadfuncs.go
@@ -8,6 +8,16 @@ import (
 	"github.com/ungerik/go-fs"
 )
 
+func LoadJSON[T any](file fs.File) (config T, err error) {
+	err = file.ReadJSON(context.Background(), &config)
+	return config, err
+}
+
+func LoadXML[T any](file fs.File) (config T, err error) {
+	err = file.ReadXML(context.Background(), &config)
+	return config, err
+}
+
 func LoadString[T ~string](file fs.File) (T, error) {
 	str, err := file.ReadAllString(context.Background())
 	if err != nil {
